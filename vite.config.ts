564cf6c45d860c6e4f0ab6cfcb481217207524a6
Vineeth Achari <vineethvp.882@gmail.com>
2026-06-17 23:06:01 +0530

    npm build

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/FPSC/",
+  base: '/First-Picture-Show-Company/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
