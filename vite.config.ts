8808caeae8732603fdea5105d7785014cceac158
Vineeth Achari <vineethvp.882@gmail.com>
2026-06-17 20:41:44 +0530

    Change base to /FPSC/

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/firstpictureshowcompany/",
+  base: "/FPSC/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
